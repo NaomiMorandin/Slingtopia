d5dd0cbd48f471927769adf3829f1eba3bf50c50
sae1034630-admin <1034630@student.sae.edu.au>
2025-07-09 14:07:33 +0930

    power calc

--- Assets/Scripts/Characters/Player.cs
+++ Assets/Scripts/Characters/Player.cs
@@ -6,28 +6,22 @@ public class Player : MonoBehaviour
 {
     [Header("Object")]
     [SerializeField] private GameObject obj;
+    [SerializeField] private Transform firePoint;
     [Space(1)]
 
     [Header("Launch")]
     [SerializeField] private float chargeRate;
-    //[SerializeField] private float chargeMax;
-    [SerializeField] private float minForce;
+    [SerializeField] private float chargeMaxForce;
+    [SerializeField] Vector3 maxLaunchForce = new Vector3(0f, 0f, 10f);
+    [SerializeField] Vector3 minLaunchForce = new Vector3(0f, 0f, 1f);
     [SerializeField] private float launchForce;
     [Space(1)]
 
     [Header("Dragging")]
     [SerializeField] private bool isDragging;
-    private Camera cam;
-    private float startXPos;
-    private float startYPos;
-
-    private Vector3 mousePos;
-    private Vector3 objectVector;
 
     private void Start()
     {
-        cam = Camera.main;
-        mousePos = Input.mousePosition;
 
         if (obj == null)
         {
@@ -49,34 +43,41 @@ public class Player : MonoBehaviour
         {
             isDragging = true;
             launchForce += Time.deltaTime * chargeRate;
+            launchForce = Mathf.Min(launchForce, chargeMaxForce);
         }
 
         if (Input.GetKeyUp(KeyCode.Mouse0))
         {
-            LaunchObject(objectVector);
+            float normalizedForce = Mathf.Clamp01(launchForce / chargeMaxForce);
+            LaunchObject(normalizedForce);
         }
     }
 
-    private void LaunchObject(Vector3 pos)
+    private void LaunchObject(float normal)
     {
-        var launchedObject = Instantiate(obj, transform.position, Quaternion.identity);
+        var launchedObject = Instantiate(obj, firePoint.position, Quaternion.identity);
         var rb = launchedObject.GetComponent<Rigidbody>();
  
         if(rb != null )
         {
-            rb.AddForce(Vector3.forward * (minForce * launchForce), ForceMode.Impulse);
+            Vector3 calcForce = CalculatedLaunchForce(normal);
+            Vector3 launchDir = Vector3.forward * calcForce.z + Vector3.up * (calcForce.z * .5f);
+            rb.AddForce(launchDir, ForceMode.Impulse);
         }
 
         isDragging = false;
         launchForce = 0f;
     }
+
+    public Vector3 CalculatedLaunchForce(float normalizedForce)
+    {
+        return Vector3.Lerp(minLaunchForce, maxLaunchForce, normalizedForce);
+    }
+
     #endregion
 }
 
-//input
-//power up shot
-//dragging
-//applying force to object when let go
+//maxLauchForce.z = 30f
 
 
 //charge shot - https://www.bing.com/videos/riverview/relatedvideo?q=unity+charging+shot&mid=C39AEDEB449F0BEED183C39AEDEB449F0BEED183&FORM=VIRE
