fa42437ffecd0ed101c41ee2aff322dfc6d27a29
SAE1\1034630 <1034630@student.sae.edu.au>
2025-07-10 10:35:33 +0930

    line renderer work

---
+++ Assets/Scripts/Game/LineRenderTest.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class LineRenderTest : MonoBehaviour
+{
+    [Header("String")]
+    [SerializeField] private Transform startPoint;
+    [SerializeField] private Transform endPoint;
+
+
+    [SerializeField] private LineRenderer lineRenderer;
+
+
+    private void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.positionCount = 2;
+    }
+
+    private void Update()
+    {
+        if (startPoint && endPoint != null)
+        {
+            lineRenderer.SetPosition(0, startPoint.position);
+            lineRenderer.SetPosition(1, endPoint.position);
+        }
+
+    }
+}
